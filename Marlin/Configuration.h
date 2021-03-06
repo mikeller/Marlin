0f3cb481138be7a1406140fd25d9deb69666f73e
mikeller <github@ike.ch>
2021-03-07 12:11:34 +1300

    Changed build configuration to ANET A8.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -139,7 +139,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_ANET_10
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
